28b3d5a3e7fb9798fb597529f716f8570a6c7930
Otávio Pace <otaviopp8@gmail.com>
2020-07-12 23:49:19 -0300

    graph: create protocol and migrate to deftype

--- src/concurrent_graph/core.clj
+++ src/concurrent_graph/core.clj
@@ -1,11 +1,30 @@
-(ns concurrent-graph.core)
+(ns concurrent-graph.core
+  (:import [clojure.lang ILookup]))
 
-(defn concurrent-graph []
-  (atom {}))
+(defprotocol Graph
+  (add-vertex [this vertex])
+  (add-edge [this v1 v2]))
 
-(defn add-vertex [graph vertex]
-  (swap! graph assoc vertex #{}))
+(deftype ConcurrentGraph [graph]
+  Graph
+  (add-vertex [_ vertex]
+    (swap! graph assoc vertex #{}))
+  (add-edge [_ v1 v2]
+    (swap! graph #(update % v1 conj v2))
+    (swap! graph #(update % v2 conj v1)))
 
-(defn add-edge [graph v1 v2]
-  (swap! graph #(update % v1 conj v2))
-  (swap! graph #(update % v2 conj v1)))
+  ILookup
+  (valAt [this k]
+    (.valAt this k nil))
+  (valAt [_ k not-found]
+    (if (= k :graph)
+      @graph
+      (if (nil? not-found)
+        (ex-info "The only key ConcurrentGraph has is :graph" {})
+        not-found))))
+
+(defn concurrent-graph
+  ([]
+   (concurrent-graph {}))
+  ([graph]
+    (->ConcurrentGraph (atom graph))))
--- test/concurrent_graph/core_test.clj
+++ test/concurrent_graph/core_test.clj
@@ -5,14 +5,14 @@
 
 (deftest create-concurrent-graph
   (testing "constructs a new concurrent-graph"
-    (is (true? (= @(concurrent-graph) {})))
-    (is (true? (= @(concurrent-graph) @(concurrent-graph))))))
+    (is (true? (= (:graph (concurrent-graph)) {})))
+    (is (true? (= (:graph (concurrent-graph)) (:graph (concurrent-graph)))))))
 
 (deftest adds-vertex-to-graph
   (testing "adds vertex to graph"
     (let [graph (concurrent-graph)]
       (is (true? (= (add-vertex graph :A) {:A #{}})))
-      (is (true? (= @graph {:A #{}}))))))
+      (is (true? (= (:graph graph) {:A #{}}))))))
 
 (deftest adds-edge-to-graph
   (testing "adds edge to graph"
@@ -20,4 +20,4 @@
       (add-vertex graph :A)
       (add-vertex graph :B)
       (is (true? (= (add-edge graph :A :B) {:A #{:B} :B #{:A}})))
-      (is (true? (= @graph {:A #{:B} :B #{:A}}))))))
+      (is (true? (= (:graph graph) {:A #{:B} :B #{:A}}))))))
